1cfb6d52e248598ca1722879d20eec86842f9fdf
MariaMagdalina <metodist_rmcc@mail.ru>
2022-08-21 11:06:02 +0300

    enter new file

---
+++ day1.4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter user name");
+string username=Console.ReadLine();
+if(username == "Маша")
+{
+   Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+   Console.Write("Привет,");
+   Console.WriteLine(username);
+}
